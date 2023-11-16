05a86743b846bc5696f5cd62dd249f6aa55e0fad
khylaehza <khondrade@gmail.com>
2023-11-17 02:56:15 +0800

    feat: fix not found page

--- src/routes/NotFoundPage.jsx
+++ src/routes/NotFoundPage.jsx
@@ -1,41 +1,61 @@
 import React from 'react';
 import { Box, Center, Heading, Text, Button, Image } from '@chakra-ui/react';
-import {IoIosArrowRoundBack} from 'react-icons/io'
+import { IoIosArrowRoundBack } from 'react-icons/io';
 
 const NotFoundPage = () => {
+	const GoBack = () => {
+		window.history.back();
+	};
+	return (
+		<Center
+			h='100vh'
+			bgColor={'w.100'}
+		>
+			<Box
+				textAlign='center'
+				display={'flex'}
+				flexDir={'column'}
+			>
+				<Image
+					alignSelf={'center'}
+					src={'/gifs/maintenance/404error.gif'}
+					alt='404 - Not Found GIF'
+					style={{
+						width: '180px',
+						height: '180px',
+						marginBottom: '15px',
+					}}
+				/>
+				<Heading
+					fontSize='6xl'
+					color='r.100'
+					mb={4}
+				>
+					404 - Page Not Found
+				</Heading>
+				<Text
+					fontSize='xl'
+					color='gray.500'
+					mb={8}
+				>
+					The requested page does not exist.
+				</Text>
 
-  const GoBack = ()=>{
-    window.history.back()
-  }
-  return (
-    <Center h="100vh">
-      <Box textAlign="center" display={'flex'} flexDir={'column'}>
-        <Image
-          alignSelf={'center'}
-          src={"../../public/gifs/maintenance/404error.gif"} 
-          alt="404 - Not Found GIF"
-          style={{ width: '180px', height: '180px', marginBottom: '15px' }}
-        />
-        <Heading fontSize="6xl" color="red.500" mb={4}>
-          404 - Page Not Found
-        </Heading>
-        <Text fontSize="xl" color="gray.500" mb={8}>
-          The requested page does not exist.
-        </Text>
-        
-          <Button
-            w={'45%'}
-            alignSelf={'center'}
-            colorScheme="blue"
-            leftIcon={<IoIosArrowRoundBack/>} 
-            size="lg"
-            onClick={GoBack}
-          >
-            Go Back to Previous Page
-          </Button>
-      </Box>
-    </Center>
-  );
+				<Button
+					w={'45%'}
+					alignSelf={'center'}
+					colorScheme='blue'
+					bgColor='b.200'
+					_hover={{ bgColor: 'b.300' }}
+					leftIcon={<IoIosArrowRoundBack />}
+					size='lg'
+					onClick={GoBack}
+				>
+					Go Back to Previous Page
+				</Button>
+			</Box>
+		</Center>
+	);
 };
 
 export default NotFoundPage;
