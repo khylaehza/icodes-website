88378cbad034dc0ab9149e7d35d922820a1a60bb
khylaehza <khondrade@gmail.com>
2023-11-28 10:56:48 +0800

    feat: fix pdf soa peso

--- src/customs/CusSoaPDF.jsx
+++ src/customs/CusSoaPDF.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-	Page,
-	Text,
-	View,
-	Document,
-	StyleSheet,
-} from '@react-pdf/renderer';
-
-
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
 	page: {
@@ -18,7 +10,7 @@ const styles = StyleSheet.create({
 		padding: 10,
 		flexGrow: 1,
 	},
-  
+
 	title: {
 		fontSize: 15,
 		marginBottom: 10,
@@ -26,70 +18,68 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		color: '#0A2542',
 	},
-  
-	info:{
-      flexDirection: 'row',
-	  justifyContent: 'space-between',
-    },
+
+	info: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
 
 	name: {
 		fontSize: 12,
 		fontWeight: 500,
 		textAlign: 'center',
-
 	},
 
-  	label: {
+	label: {
 		fontSize: 12,
 		fontWeight: 700,
 		textAlign: 'center',
 	},
-
 
 	text: {
 		fontSize: 11,
 		fontWeight: 'normal',
 	},
 
-  	table: {
-        display: 'table',
-        width: '100%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: '#bfbfbf',
-        marginTop: 10,
-    },
-    tableRow: {
-        flexDirection: 'row',
-    },
-    tableHeader: {
-        flex: 1,
-        backgroundColor: '#f0f0f0',
-        fontWeight: 'bold',
-        padding: 5,
-        textAlign: 'center',
-        borderTopWidth: 0,
-        borderBottomWidth: 1,
-        borderRightWidth: 0,
-        borderLeftWidth: 0,
-        borderColor: '#bfbfbf',
+	table: {
+		display: 'table',
+		width: '100%',
+		borderStyle: 'solid',
+		borderWidth: 1,
+		borderColor: '#bfbfbf',
+		marginTop: 10,
+	},
+	tableRow: {
+		flexDirection: 'row',
+	},
+	tableHeader: {
+		flex: 1,
+		backgroundColor: '#f0f0f0',
+		fontWeight: 'bold',
+		padding: 5,
+		textAlign: 'center',
+		borderTopWidth: 0,
+		borderBottomWidth: 1,
+		borderRightWidth: 0,
+		borderLeftWidth: 0,
+		borderColor: '#bfbfbf',
 		fontSize: 12,
 		color: '#0A2542',
-    },
-    tableCell: {
-        flex: 1,
-        borderStyle: 'solid',
-        borderWidth: 0,
-        borderTopWidth: 0,
-        borderBottomWidth: 1,
-        padding: 5,
-        textAlign: 'center',
-        borderColor: '#bfbfbf',
+	},
+	tableCell: {
+		flex: 1,
+		borderStyle: 'solid',
+		borderWidth: 0,
+		borderTopWidth: 0,
+		borderBottomWidth: 1,
+		padding: 5,
+		textAlign: 'center',
+		borderColor: '#bfbfbf',
 		fontSize: 11,
-    },
-    alternateRow: {
-        backgroundColor: '#f9f9f9',
-    },
+	},
+	alternateRow: {
+		backgroundColor: '#f9f9f9',
+	},
 	additionalSection: {
 		marginTop: 5,
 		backgroundColor: '#f0f0f0',
@@ -98,100 +88,120 @@ const styles = StyleSheet.create({
 		borderColor: '#bfbfbf',
 		justifyContent: 'space-between',
 		textAlign: 'left',
-		flexDirection: 'row'
+		flexDirection: 'row',
 	},
-  
-  
 });
-const CusSoaPDF = ({personalDet,table,remainPer,remainUnit,remainOthers,totalRemain}) =>{
-    return(
-     <Document>
-		<Page
-			size='A4'
-			style={styles.page}
-		>
-			  
-			<View style={styles.section} >
-				<Text style={styles.title}>
-					Statement of Account
-				</Text> 
-				<View style={styles.info}>
-                        {personalDet.map((item, index) => (
-                            <Text key={index} style={styles.label}>
-                                <Text style={styles.text}>
-                                    {Object.keys(item)[0]}:
-                                </Text>{' '}
-                                {Object.values(item)[0]}
+const CusSoaPDF = ({
+	personalDet,
+	table,
+	remainPer,
+	remainUnit,
+	remainOthers,
+	totalRemain,
+}) => {
+	return (
+		<Document>
+			<Page
+				size='A4'
+				style={styles.page}
+			>
+				<View style={styles.section}>
+					<Text style={styles.title}>Statement of Account</Text>
+					<View style={styles.info}>
+						{personalDet.map((item, index) => (
+							<Text
+								key={index}
+								style={styles.label}
+							>
+								<Text style={styles.text}>
+									{Object.keys(item)[0]}:
+								</Text>{' '}
+								{Object.values(item)[0]}
 								{'     '}
-                            </Text>
-                        ))}
-                </View>
-			  
-			
-			   <View style={styles.table}>
-					{/* Table Header */}
-					<View style={styles.tableRow}>
-						<View style={styles.tableHeader}>
-							<Text>NO.</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>DATE</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>AMOUNT</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>OTHER</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>TOTAL</Text>
-						</View>
+							</Text>
+						))}
 					</View>
-					{/* Table Rows */}
-					{table.map((data,key)=>(
-						<View key={key} style={[
-							styles.tableRow,
-							key % 2 !== 0 ? styles.alternateRow : null, // Apply alternateRow style to odd-indexed rows
-						  ]}
-						>
-							<View style={styles.tableCell}>
-								<Text>{data.num}</Text>
+
+					<View style={styles.table}>
+						{/* Table Header */}
+						<View style={styles.tableRow}>
+							<View style={styles.tableHeader}>
+								<Text>NO.</Text>
 							</View>
-							<View style={styles.tableCell}>
-								<Text>{data.month}</Text>
+							<View style={styles.tableHeader}>
+								<Text>DATE</Text>
 							</View>
-							<View style={styles.tableCell}>
-								<Text>{data.unit.replace(/±/,'\u20B1')}</Text>
+							<View style={styles.tableHeader}>
+								<Text>AMOUNT</Text>
 							</View>
-							<View style={styles.tableCell}>
-								<Text>{data.others.replace(/±/,'\u20B1')}</Text>
+							<View style={styles.tableHeader}>
+								<Text>OTHER</Text>
 							</View>
-							<View style={styles.tableCell}>
-								<Text>{data.total.replace(/±/,'\u20B1')}</Text>
+							<View style={styles.tableHeader}>
+								<Text>TOTAL</Text>
 							</View>
+						</View>
+						{/* Table Rows */}
+
+						{table.map((data, key) => {
+							let other =
+								data.others && data.others.replace(/₱/, 'P');
+							let unit = data.unit && data.unit.replace(/₱/, 'P');
+							let total =
+								data.total && data.total.replace(/₱/, 'P');
+
+							return (
+								<View
+									key={key}
+									style={[
+										styles.tableRow,
+										key % 2 !== 0
+											? styles.alternateRow
+											: null, // Apply alternateRow style to odd-indexed rows
+									]}
+								>
+									<View style={styles.tableCell}>
+										<Text>{data.num}</Text>
+									</View>
+									<View style={styles.tableCell}>
+										<Text>{data.month}</Text>
+									</View>
+									<View style={styles.tableCell}>
+										<Text>{unit}</Text>
+									</View>
+									<View style={styles.tableCell}>
+										<Text>{other}</Text>
+									</View>
+									<View style={styles.tableCell}>
+										<Text>{total}</Text>
+									</View>
+								</View>
+							);
+						})}
 					</View>
-					))}
-				</View>
 
-				<View style={styles.additionalSection}>
-					<Text style={styles.label}>REMAINING UNIT BALANCE ({remainPer}% of TCP):</Text>
-					<Text style={styles.name}>₱ {remainUnit}</Text>
+					<View style={styles.additionalSection}>
+						<Text style={styles.label}>
+							REMAINING UNIT BALANCE ({remainPer}% of TCP):
+						</Text>
+						<Text style={styles.name}>P {remainUnit}</Text>
+					</View>
+					<View style={styles.additionalSection}>
+						<Text style={styles.label}>
+							REMAINING OTHER BALANCE ({remainPer}% of OC):
+						</Text>
+						<Text style={styles.name}>P {remainOthers}</Text>
+					</View>
+					<View style={styles.additionalSection}>
+						<Text style={styles.label}>
+							TOTAL REMAINING BALANCE:
+						</Text>
+						<Text style={styles.name}>P {totalRemain}</Text>
+					</View>
 				</View>
-				<View style={styles.additionalSection}>
-					<Text style={styles.label}>REMAINING OTHER BALANCE ({remainPer}% of OC):</Text>
-					<Text style={styles.name}>₱ {remainOthers}</Text>
-				</View>
-				<View style={styles.additionalSection}>
-					<Text style={styles.label}>TOTAL REMAINING BALANCE:</Text>
-					<Text style={styles.name}>₱ {totalRemain}</Text>
-				</View>
-			  
-			</View>
-		  
-			  
-		</Page>
-	</Document>
-    )
-}
+			</Page>
+		</Document>
+	);
+};
 
-export default CusSoaPDF
+export default CusSoaPDF;
--- src/customs/CusTransactionPDF.jsx
+++ src/customs/CusTransactionPDF.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-	Page,
-	Text,
-	View,
-	Document,
-	StyleSheet,
-} from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 import moment from 'moment';
 
@@ -18,7 +12,7 @@ const styles = StyleSheet.create({
 		padding: 10,
 		flexGrow: 1,
 	},
-  
+
 	title: {
 		fontSize: 15,
 		marginBottom: 10,
@@ -26,25 +20,24 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		color: '#0A2542',
 	},
-  
-  	section2:{
-      color: '#0A2542',
-      flexDirection: 'row'
-    },
 
-	info:{
-      flexDirection: 'row',
-	  justifyContent: 'space-between',
-    },
+	section2: {
+		color: '#0A2542',
+		flexDirection: 'row',
+	},
+
+	info: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
 
 	name: {
 		fontSize: 12,
 		fontWeight: 500,
 		textAlign: 'center',
-
 	},
 
-  	label: {
+	label: {
 		fontSize: 12,
 		fontWeight: 700,
 		textAlign: 'center',
@@ -67,45 +60,45 @@ const styles = StyleSheet.create({
 		width: '100%',
 		marginBottom: 10,
 	},
-  	    table: {
-        display: 'table',
-        width: '100%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: '#bfbfbf',
-        marginTop: 10,
-    },
-    tableRow: {
-        flexDirection: 'row',
-    },
-    tableHeader: {
-        flex: 1,
-        backgroundColor: '#f0f0f0',
-        fontWeight: 'bold',
-        padding: 5,
-        textAlign: 'center',
-        borderTopWidth: 0,
-        borderBottomWidth: 1,
-        borderRightWidth: 0,
-        borderLeftWidth: 0,
-        borderColor: '#bfbfbf',
+	table: {
+		display: 'table',
+		width: '100%',
+		borderStyle: 'solid',
+		borderWidth: 1,
+		borderColor: '#bfbfbf',
+		marginTop: 10,
+	},
+	tableRow: {
+		flexDirection: 'row',
+	},
+	tableHeader: {
+		flex: 1,
+		backgroundColor: '#f0f0f0',
+		fontWeight: 'bold',
+		padding: 5,
+		textAlign: 'center',
+		borderTopWidth: 0,
+		borderBottomWidth: 1,
+		borderRightWidth: 0,
+		borderLeftWidth: 0,
+		borderColor: '#bfbfbf',
 		fontSize: 10,
 		color: '#0A2542',
-    },
-    tableCell: {
-        flex: 1,
-        borderStyle: 'solid',
-        borderWidth: 0,
-        borderTopWidth: 0,
-        borderBottomWidth: 1,
-        padding: 5,
-        textAlign: 'center',
-        borderColor: '#bfbfbf',
+	},
+	tableCell: {
+		flex: 1,
+		borderStyle: 'solid',
+		borderWidth: 0,
+		borderTopWidth: 0,
+		borderBottomWidth: 1,
+		padding: 5,
+		textAlign: 'center',
+		borderColor: '#bfbfbf',
 		fontSize: 10,
-    },
-    alternateRow: {
-        backgroundColor: '#f9f9f9',
-    },
+	},
+	alternateRow: {
+		backgroundColor: '#f9f9f9',
+	},
 	additionalSection: {
 		marginTop: 5,
 		backgroundColor: '#f0f0f0',
@@ -114,165 +107,150 @@ const styles = StyleSheet.create({
 		borderColor: '#bfbfbf',
 		justifyContent: 'space-between',
 	},
-  
-  
 });
-const CusTransactionPDF = ({personalDet,amountDet,table,deduct,remainUnit,remainOthers,totalRemain}) =>{
-    return(
-     <Document>
-		<Page
-			size='A4'
-			style={styles.page}
-		>
-			  
-			<View style={styles.section} >
-				<Text style={styles.title}>
-					Transaction List
-				</Text> 
-				<View style={styles.info}>
-                        {personalDet.map((item, index) => (
-                            <Text key={index} style={styles.label}>
-                                <Text style={styles.text}>
-                                    {Object.keys(item)[0]}:
-                                </Text>{' '}
-                                {Object.values(item)[0]}
+const CusTransactionPDF = ({
+	personalDet,
+	amountDet,
+	table,
+	deduct,
+	remainUnit,
+	remainOthers,
+	totalRemain,
+}) => {
+	return (
+		<Document>
+			<Page
+				size='A4'
+				style={styles.page}
+			>
+				<View style={styles.section}>
+					<Text style={styles.title}>Transaction List</Text>
+					<View style={styles.info}>
+						{personalDet.map((item, index) => (
+							<Text
+								key={index}
+								style={styles.label}
+							>
+								<Text style={styles.text}>
+									{Object.keys(item)[0]}:
+								</Text>{' '}
+								{Object.values(item)[0]}
 								{'     '}
-                            </Text>
-                        ))}
+							</Text>
+						))}
 
-                        {amountDet.map((item, index) => (
-                            <Text key={index} style={styles.label}>
-                                <Text style={styles.text}>
-                                    {Object.keys(item)[0]}:
-                                </Text>₱{' '}
-                                    {new Intl.NumberFormat(
-										'en-US',
-										{
-											maximumFractionDigits: 2,
-											minimumFractionDigits: 2,
-										}
-									).format(
-										Object.values(item)[0]
-									)}
-                            </Text>
-                        ))}
-
-
-                </View>
-			  
-			
-			   <View style={styles.table}>
-					{/* Table Header */}
-					<View style={styles.tableRow}>
-						<View style={styles.tableHeader}>
-							<Text>No.</Text>
-						</View>
-                        <View style={styles.tableHeader}>
-							<Text>Transaction NO.</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>DUE DATE</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>DATE PAID</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>MODE OF PAYMENT</Text>
-						</View>
-						<View style={styles.tableHeader}>
-							<Text>AMOUNT</Text>
-						</View>
-                        <View style={styles.tableHeader}>
-							<Text>RECEIPT NO.</Text>
-						</View>
-                        <View style={styles.tableHeader}>
-							<Text>REMAINING BALANCE.</Text>
-						</View>
+						{amountDet.map((item, index) => (
+							<Text
+								key={index}
+								style={styles.label}
+							>
+								<Text style={styles.text}>
+									{Object.keys(item)[0]}:
+								</Text>
+								P{' '}
+								{new Intl.NumberFormat('en-US', {
+									maximumFractionDigits: 2,
+									minimumFractionDigits: 2,
+								}).format(Object.values(item)[0])}
+							</Text>
+						))}
 					</View>
-					{/* Table Rows */}
-					{table.map((data,key)=>{
-                        if (data.status == 'Paid') {
-                            deduct += parseFloat(
-                                data.amountPaid.replace(
-                                    /,/g,
-                                    ''
-                                )
-                            );
-                            return(
-                                <View key={key} style={[
-                                    styles.tableRow,
-                                    key % 2 !== 0 ? styles.alternateRow : null, // Apply alternateRow style to odd-indexed rows
-                                  ]}
-                                >
-                                    <View style={styles.tableCell}>
-                                        <Text>{data.num}</Text>
-                                    </View>
-                                    <View style={styles.tableCell}>
-                                        <Text>{data.transactId}</Text>
-                                    </View>
-                                    <View style={styles.tableCell}>
-                                        <Text>{moment(
-													data.month
-												).format(
+
+					<View style={styles.table}>
+						{/* Table Header */}
+						<View style={styles.tableRow}>
+							<View style={styles.tableHeader}>
+								<Text>No.</Text>
+							</View>
+							<View style={styles.tableHeader}>
+								<Text>Transaction NO.</Text>
+							</View>
+							<View style={styles.tableHeader}>
+								<Text>DUE DATE</Text>
+							</View>
+							<View style={styles.tableHeader}>
+								<Text>DATE PAID</Text>
+							</View>
+							<View style={styles.tableHeader}>
+								<Text>MODE OF PAYMENT</Text>
+							</View>
+							<View style={styles.tableHeader}>
+								<Text>AMOUNT</Text>
+							</View>
+							<View style={styles.tableHeader}>
+								<Text>RECEIPT NO.</Text>
+							</View>
+							<View style={styles.tableHeader}>
+								<Text>REMAINING BALANCE.</Text>
+							</View>
+						</View>
+						{/* Table Rows */}
+						{table.map((data, key) => {
+							if (data.status == 'Paid') {
+								deduct += parseFloat(
+									data.amountPaid.replace(/,/g, '')
+								);
+								return (
+									<View
+										key={key}
+										style={[
+											styles.tableRow,
+											key % 2 !== 0
+												? styles.alternateRow
+												: null, // Apply alternateRow style to odd-indexed rows
+										]}
+									>
+										<View style={styles.tableCell}>
+											<Text>{data.num}</Text>
+										</View>
+										<View style={styles.tableCell}>
+											<Text>{data.transactId}</Text>
+										</View>
+										<View style={styles.tableCell}>
+											<Text>
+												{moment(data.month).format(
 													'DD-MM-YYYY'
-												)}  </Text>
-                                    </View>
-                                    <View style={styles.tableCell}>
-                                        <Text>{moment(
-													data.datePaid
-												).format(
+												)}{' '}
+											</Text>
+										</View>
+										<View style={styles.tableCell}>
+											<Text>
+												{moment(data.datePaid).format(
 													'DD-MM-YYYY'
-												)}  </Text>
-                                    </View>
-                                    <View style={styles.tableCell}>
-                                        <Text>{data.paymentMode}</Text>
-                                    </View>
-                                    <View style={styles.tableCell}>
-                                        <Text>{'₱ ' +  data.amountPaid}</Text>
-                                    </View>
-                                    <View style={styles.tableCell}>
-                                        <Text>{data.receiptNo}</Text>
-                                    </View>
-                                    <View style={styles.tableCell}>
-                                        <Text>{`₱ ${new Intl.NumberFormat(
-													'en-US',
-													{
-														maximumFractionDigits: 2,
-														minimumFractionDigits: 2,
-													}
-												).format(
-													amountDet[2]
-														.Total -
-														deduct
-												)}`}</Text>
-                                    </View>
-                            </View>
-                            )
-                            }
-                        })
-                    }
-
+												)}{' '}
+											</Text>
+										</View>
+										<View style={styles.tableCell}>
+											<Text>{data.paymentMode}</Text>
+										</View>
+										<View style={styles.tableCell}>
+											<Text>
+												{'P ' + data.amountPaid}
+											</Text>
+										</View>
+										<View style={styles.tableCell}>
+											<Text>{data.receiptNo}</Text>
+										</View>
+										<View style={styles.tableCell}>
+											<Text>{`P ${new Intl.NumberFormat(
+												'en-US',
+												{
+													maximumFractionDigits: 2,
+													minimumFractionDigits: 2,
+												}
+											).format(
+												amountDet[2].Total - deduct
+											)}`}</Text>
+										</View>
+									</View>
+								);
+							}
+						})}
+					</View>
 				</View>
+			</Page>
+		</Document>
+	);
+};
 
-				{/* <View style={styles.additionalSection}>
-					<Text style={styles.label}>REMAINING UNIT BALANCE ({remainPer}% of TCP):</Text>
-					<Text style={styles.name}>₱ {remainUnit}</Text>
-				</View>
-				<View style={styles.additionalSection}>
-					<Text style={styles.label}>REMAINING OTHER BALANCE ({remainPer}% of OC):</Text>
-					<Text style={styles.name}>₱ {remainOthers}</Text>
-				</View>
-				<View style={styles.additionalSection}>
-					<Text style={styles.label}>TOTAL REMAINING BALANCE:</Text>
-					<Text style={styles.name}>₱ {totalRemain}</Text>
-				</View> */}
-			  
-			</View>
-		  
-			  
-		</Page>
-	</Document>
-    )
-}
-
-export default CusTransactionPDF
+export default CusTransactionPDF;
